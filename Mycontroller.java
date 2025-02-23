298f24376718002253179a58d1bd46a3612f06c1
Pankaj <pankaj@gopis-Mac-mini.local>
2025-02-23 13:51:36 +0530

    inital setup for spring

---
+++ Mycontroller.java
@@ -0,0 +1,2 @@
+public class Mycontroller {
+}
---
+++ src/main/java/com/entity/Course.java
@@ -0,0 +1,51 @@
+package com.entity;
+
+public class Course {
+    private long id;
+    private String title;
+    private String description;
+
+    public Course(String description, String title, long id) {
+        super();
+        this.description = description;
+        this.title = title;
+        this.id = id;
+    }
+
+    public Course() {
+        super();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
